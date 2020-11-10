5a2e3bfb05c5535010eb1178701e0334ee87ab4c
Adurotimi Joshua <adurotimijoshua@gmail.com>
2020-11-10 12:54:22 +0100

    Build question form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -23,11 +23,12 @@ class QuestionsController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        $questions = new Question();
+        return view('questions.create', compact('questions'));
     }
 
     /**
