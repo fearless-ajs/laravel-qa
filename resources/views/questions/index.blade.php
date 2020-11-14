9250323dfcd0e8c851fe03898a4d23260fa8b5c5
Adurotimi Joshua <adurotimijoshua@gmail.com>
2020-11-14 02:43:51 +0100

    Refactoring our views

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,47 +18,9 @@
                 <div class="card-body">
                     @include('layouts._messages')
                     @forelse($questions as $question)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{$question->votes_count }}</strong>{{Str::plural('vote', $question->votes_count)}}
-                                </div>
-                                <div class="status {{ $question->status }}">
-                                    <strong>{{$question->answers_count }}</strong>{{Str::plural('answer', $question->answers_count)}}
-                                </div>
-                                <div class="view">
-                                   {{$question->views . "  " .  Str::plural('view', $question->views)}}
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                    <div class="ml-auto">
-                                        @can('update-question', $question)
-                                           <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endif
-                                        @can('delete-question', $question)
-                                        <form class="form-delete" method="post" action="{{route('questions.destroy', $question->id)}}">
-                                            @method('delete')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure')">Delete</button>
-                                        </form>
-                                        @endif
-                                    </div>
-                                </div>
-{{--                                <h3 class="mt-0"><a href="{{$question->url}}"> {{$question->title}}</a></h3>--}}
-                                <p class="lead">
-                                    Asked by
-                                    <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                    <small class="text-muted">{{$question->created_date}}</small>
-                                </p>
-                                <div class="exerpt">
-                                    {{$question->exerpt}}
-                                </div>
 
-                            </div>
-                        </div>
-                        <hr>
+                       @include('questions._exerpt')
+
                     @empty
                         <div class="alert alert-warning">
                             <strong>Sorry!</strong> There are no questions available
