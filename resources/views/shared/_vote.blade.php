e6b7bc867984fadaec7402cc8fe4ff37dfddf209
Adurotimi Joshua <adurotimijoshua@gmail.com>
2020-11-22 19:34:36 +0100

    Create accept answer component

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -37,8 +37,6 @@
     @if($model instanceof \App\Models\Question)
        <Favorite :question="{{ $model }}"></Favorite>
     @elseif($model instanceof \App\Models\Answer)
-        @include('shared._accept', [
-        'model' => $model,
-        ])
+        <accept :answer="{{ $model }}"></accept>
     @endif
 </div>
